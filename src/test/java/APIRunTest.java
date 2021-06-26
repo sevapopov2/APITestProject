1a0767ebb0c97037bb1829827b247e3df934d341
Vsevolod Popov <sevapopov13@gmail.com>
2021-06-26 12:22:45 +0300

    api run test is modified.

--- src/test/java/APIRunTest.java
+++ src/test/java/APIRunTest.java
@@ -2,6 +2,7 @@ import static io.restassured.RestAssured.given;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import io.restassured.response.Response;
@@ -10,8 +11,8 @@ public class APIRunTest {
     private static Response response;
     private static final Logger LOGGER = LogManager.getLogger(APIRunTest.class);
 
-    @Test
-    public void setup() {
+    @BeforeAll
+    public static void setup() {
         try {
             response = given().get(Consts.URL + Consts.LIVE_ENDPOINT + Consts.TOKEN);
             System.out.println(response.asString());
@@ -21,6 +22,14 @@ public class APIRunTest {
         }
         catch (Exception err) {
             System.out.println("You have not supplied a valid API Access Key.");
+            response.then().statusCode(101);
         }
+    }
+
+    @Test
+    public void apiRunTest() {
+        LOGGER.info(response.asString());
+        response.then().statusCode(200);
+        LOGGER.info(response.);
     }
 }
